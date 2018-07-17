650e472205a7e3346096e135ece7ac2e932d383f
Kai Schwarz <kschwarz@hexonet.net>
2018-07-17 18:12:26 +0200

    backup current rewrite status

---
+++ lib/HEXONET/apiconnector/connection.pm
@@ -0,0 +1,82 @@
+package HEXONET::Connection;
+
+use strict;
+use warnings;
+use HEXONET;
+use HEXONET::Response;
+use HEXONET::Util;
+use LWP::UserAgent;
+
+
+sub new {
+	my $class = shift;
+	my $self = {@_};
+	foreach my $key (%$self) {
+		my $value = $self->{$key};
+		delete $self->{$key};
+		$self->{lc $key} = $value;
+	}
+	return bless $self, $class;
+}
+
+
+sub call {
+	my $self = shift;
+	my $command = shift;
+	my $config = shift;
+	return HEXONET::Response->new($self->call_raw($command, $config));
+}
+
+
+sub call_raw {
+	my $self = shift;
+	my $command = shift;
+	my $config = shift;
+
+	$config = {} if !defined $config;
+	$config = { User => $config } if (defined $config) && (!ref $config);
+
+	return $self->call_raw_http($command, $config);
+}
+
+
+sub call_raw_http {
+	my $self = shift;
+	my $command = shift;
+	my $config = shift;
+
+	my $ua = $self->_get_useragent();
+
+	my $url = $self->{url};
+	my $post = {s_command => (scalar HEXONET::Util::command_encode($command))};
+	$post->{s_entity} = $self->{entity} if exists $self->{entity};
+	$post->{s_login} = $self->{login} if exists $self->{login};
+	$post->{s_pw} = $self->{password} if exists $self->{password};
+	$post->{s_user} = $self->{user} if exists $self->{user};
+	$post->{s_login} = $self->{login} . "!" . $self->{role} if exists $self->{role};
+
+	if ( exists $config->{user} ) {
+		if ( exists $post->{s_user} ) {
+			$post->{s_user} .= " ".$config->{user};
+		}else {
+			$post->{s_user} = $config->{user};
+		}
+	}
+
+	my $response = $self->{_useragent}->post($url, $post );
+	return $response->content();
+
+}
+
+
+sub _get_useragent {
+	my $self = shift;
+	return $self->{_useragent} if exists $self->{_useragent};
+	$self->{_useragent} = new LWP::UserAgent(
+		agent => "HEXONET-perl/$HEXONET::VERSION",
+		keep_alive => 4
+	);
+	return $self->{_useragent};
+}
+
+1;
---
+++ lib/HEXONET/apiconnector/response.pm
@@ -0,0 +1,150 @@
+package HEXONET::Response;
+
+use strict;
+use warnings;
+use HEXONET::Util;
+use overload
+  '%{}' => \&_as_hash_op,
+  '@{}' => \&as_list,
+  ;
+
+
+sub new {
+	my $class = shift;
+	my $response = shift;
+	my $self = {};
+
+	if ( (ref $response) eq "HASH" ) {
+		$self->{_response_hash} = $response;
+	}elsif ( !ref $response ) {
+		$self->{_response_string} = $response;
+	}else {
+		die "Unsupported Class: ".(ref $response);
+	}
+
+	bless $self, $class;
+	$self->{test} = 1;
+
+	return bless $self, $class;
+}
+
+
+sub as_string {
+	my $self = shift;
+
+	return $self->{_response_string};
+}
+
+
+sub _as_hash_op {
+	my $self = shift;
+
+	# Don't hide the $self hash if called from within class
+	my ($pkg) = caller 0;
+	return $self if $pkg->isa('HEXONET::Response');
+	return $self->as_hash();
+}
+
+
+sub as_hash {
+	my $self = shift;
+
+	return $self->{_response_hash} if defined $self->{_response_hash};
+	$self->{_response_hash} = HEXONET::Util::response_to_hash($self->{_response_string});
+	return $self->{_response_hash};
+}
+
+
+sub as_list_hash {
+	my $self = shift;
+
+	return $self->{_response_list_hash} if defined $self->{_response_list_hash};
+	$self->{_response_list_hash} = HEXONET::Util::response_to_list_hash($self->as_hash());
+	return $self->{_response_list_hash};
+}
+
+
+sub as_list {
+	my $self = shift;
+	my $list_hash = $self->as_list_hash();
+	if (wantarray) {
+		return @{$list_hash->{ITEMS}};
+	}
+	return $list_hash->{ITEMS};
+}
+
+
+sub code {
+	my $self = shift;
+	return $self->as_hash()->{CODE};
+}
+
+
+sub description {
+	my $self = shift;
+	return $self->as_hash()->{DESCRIPTION};
+}
+
+
+sub properties {
+	my $self = shift;
+	return $self->as_hash()->{PROPERTY};
+}
+
+
+sub runtime {
+	my $self = shift;
+	return $self->as_hash()->{RUNTIME};
+}
+
+
+sub queuetime {
+	my $self = shift;
+	return $self->as_hash()->{QUEUETIME};
+}
+
+
+sub property {
+	my $self = shift;
+	my $property = shift;
+	my $index = shift;
+	my $p = $self->as_hash()->{PROPERTY};
+	if ( defined $index ) {
+		return (LIST { undef }SCALAR { undef }) unless exists $p->{$property};
+		return $p->{$property}[$index];
+	}
+	if (wantarray) {
+		return () unless exists $p->{$property};
+		return @{$p->{$property}};
+	}
+	return (LIST { undef }SCALAR { undef }) unless exists $p->{$property};
+	return $p->{$property};
+}
+
+
+sub is_success {
+	my $self = shift;
+	return $self->as_hash()->{CODE} =~ /^2/;
+}
+
+
+sub is_tmp_error {
+	my $self = shift;
+	return $self->as_hash()->{CODE} =~ /^4/;
+}
+
+sub columns { my $self = shift; return $self->as_list_hash()->{COLUMNS}; }
+sub first { my $self = shift; return $self->as_list_hash()->{FIRST}; }
+sub last { my $self = shift; return $self->as_list_hash()->{LAST}; }
+sub count { my $self = shift; return $self->as_list_hash()->{COUNT}; }
+sub limit { my $self = shift; return $self->as_list_hash()->{LIMIT}; }
+sub total { my $self = shift; return $self->as_list_hash()->{TOTAL}; }
+sub pages { my $self = shift; return $self->as_list_hash()->{PAGES}; }
+sub page { my $self = shift; return $self->as_list_hash()->{PAGE}; }
+sub prevpage { my $self = shift; return $self->as_list_hash()->{PREVPAGE}; }
+sub prevpagefirst { my $self = shift; return $self->as_list_hash()->{PREVPAGEFIRST}; }
+sub nextpage { my $self = shift; return $self->as_list_hash()->{NEXTPAGE}; }
+sub nextpagefirst { my $self = shift; return $self->as_list_hash()->{NEXTPAGEFIRST}; }
+sub lastpagefirst { my $self = shift; return $self->as_list_hash()->{LASTPAGEFIRST}; }
+
+1;
