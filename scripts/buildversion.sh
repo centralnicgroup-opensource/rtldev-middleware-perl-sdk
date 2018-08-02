48c0529216243bd0407f8260455d00c6d258a3b9
Kai Schwarz <kschwarz@hexonet.net>
2018-08-02 10:22:58 +0200

    updated build version script

--- scripts/buildversion.sh
+++ scripts/buildversion.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 perl Makefile.PL && make && cover -test
 rm -rf blib
-ppi_version change $1 $2
+ppi_version change "$1" "$2"
 #$1 is the old version
 #$2 is the new version
+./scripts/rebuild.sh
+git add .
 git add lib
 git add t
 git commit -m "prepare new release"
