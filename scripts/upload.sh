ce094cc2ccfa1435f3f6a35ea377a6f24e896e6e
Kai Schwarz <kschwarz@hexonet.net>
2018-12-17 15:02:48 +0100

    fix(releasing): archive file name version

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cpan-upload -u "$CPAN_USER" -p "$CPAN_PASSWORD" --md5 WebService-Hexonet-Connector-"$1".tar.gz
+cpan-upload -u "$CPAN_USER" -p "$CPAN_PASSWORD" --md5 WebService-Hexonet-Connector-v"$1".tar.gz
