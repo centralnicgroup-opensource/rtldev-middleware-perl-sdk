f672d6a2b3df94388eed505688d75e2acffbf244
Kai Schwarz <kschwarz@hexonet.net>
2020-03-16 12:58:34 +0100

    ci(travis): drop out check for branch name in release process (master anyways)

--- updateVersion.sh
+++ updateVersion.sh
@@ -6,19 +6,16 @@
 
 # version format: X.Y.Z
 newversion="$1";
-branch="$2";
 
-if [ "$branch" = "master" ]; then
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" Makefile.PL
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/APIClient.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/Column.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/Record.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/Response.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/ResponseParser.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/ResponseTemplate.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/ResponseTemplateManager.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/SocketConfig.pm
-    sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" t/Hexonet-connector.t
-fi;
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" Makefile.PL
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/APIClient.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/Column.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/Record.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/Response.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/ResponseParser.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/ResponseTemplate.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/ResponseTemplateManager.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" lib/WebService/Hexonet/Connector/SocketConfig.pm
+sed -i "s/declare('v[0-9]\\+\\.[0-9]\\+\\.[0-9]\\+')/declare('v${newversion}')/g" t/Hexonet-connector.t
