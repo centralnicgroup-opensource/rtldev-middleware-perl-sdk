5e084a6e65d5082049ed36b42d54f4b53c86dc47
Kai Schwarz <kschwarz@hexonet.net>
2020-07-16 15:28:58 +0200

    build(versionupdater): reviewed sed usage for multi-file replace

--- updateVersion.sh
+++ updateVersion.sh
@@ -8,4 +8,4 @@
 newversion="$1";
 
 printf -v sed_script "s/declare('v[0-9]\+\.[0-9]\+\.[0-9]\+')/declare('v%s')/g" "${newversion}"
-sed -i -e "${sed_script}" Makefile.PL t/Hexonet-connector.t lib/WebService/Hexonet.pm lib/WebService/Hexonet/Connector.pm lib/WebService/Hexonet/*.pm
+sed -i -e "${sed_script}" t/*.t lib/WebService/*.pm lib/WebService/Hexonet/*.pm lib/WebService/Hexonet/Connector/*.pm
